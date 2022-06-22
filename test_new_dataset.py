6cd5c29e6390693b2511e12f8792b3ed046b54d8
Alexander Mehta <alexandermehta@outlook.com>
2022-06-21 18:38:03 -0700

    new training code

--- test_new_dataset.py
+++ test_new_dataset.py
@@ -1,12 +1,23 @@
 
 from aff2newdataset import Aff2CompDatasetNew
-import torchvision
+from tsav import TwoStreamAuralVisualModel
+import torch
+model = TwoStreamAuralVisualModel().cuda()
+model = model.load_state_dict('7model.pth')
 
-train_set = Aff2CompDatasetNew(root_dir='aff2_processed')
+train_set = Aff2CompDatasetNew(root_dir='aff2_processed',test_set=True)
+total = 0
+correct = 0
+for data in loop:
+    if(int(data['expressions'][0])==-1):
+        continue
+    if('clip' not in data):
+        continue
+    x = {}
+    x['clip'] = data['clip'].to(device)
+    result = model(x)
+    labels = torch.LongTensor(data['expressions']).to(device)
+    total += labels.size(0)
+    correct += (result == labels).sum().item()
 
-video_tensor = train_set.__getitem__(10)['clip']
-# print(video_tensor.size())
-video_tensor = video_tensor.permute(1,2,3,0)
-# print(video_tensor.size())
-# print(video_tensor)
-torchvision.io.write_video(filename="test_video.mp4",video_array=video_tensor[:,:,:,0:3] ,fps=3)
+print(100*correct//total)
